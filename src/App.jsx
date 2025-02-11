d9d9ddb7f5cf06e4af12ebc642662f1767dad608
Анастасія Савченко <savyle4ka13@gmail.com>
2025-02-11 08:36:51 +0000

    Refactor ContactList: replaced props with useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
-import { useSelector } from "react-redux";
-import { selectContacts } from "./redux/contactsSlice";
-import { selectFilters } from "./redux/filtersSlice";
 
 function App() {
-  const contacts = useSelector(selectContacts);
-  const searchName = useSelector(selectFilters);
-
-  const filteredContact = contacts?.filter((contact) =>
-    contact.name.toLowerCase().includes(searchName.toLowerCase())
-  );
-
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList contacts={searchName ? filteredContact : contacts} />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,22 @@
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectFilters } from "../../redux/filtersSlice";
 
-function ContactList({ contacts }) {
+function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const searchName = useSelector(selectFilters);
+
+  const filteredContact = contacts?.filter((contact) =>
+    contact.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+
+  const contactsList = searchName ? filteredContact : contacts;
+
   return (
     <ul className={s.listBox}>
-      {contacts?.map((contact) => (
+      {contactsList?.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
